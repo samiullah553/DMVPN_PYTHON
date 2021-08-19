c547c8d2d5d2eb63cfdd9bc179f914e81c00a4f2
samiullah553 <67770797+samiullah553@users.noreply.github.com>
2021-08-19 16:09:05 +0500

    Update devic_info.py

--- devic_info.py
+++ devic_info.py
@@ -1,9 +1,9 @@
 R_1 = {
          "device_type":"cisco_ios",
          "ip":"9.1.1.5",
-         "username":"admin",
-         "password":"cisco12",
-         "secret":"cisco12"
+         "username":"admin", #this is all about ssh username
+         "password":"cisco12", #ssh password
+         "secret":"cisco12" #Enable password of the cisco device
         }
 R_2 = {
          "device_type":"cisco_ios",
